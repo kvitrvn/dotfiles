f4b34d53fccb7ddb14b8c47e6366ed0fadd2db85
Benjamin Gaudé <benjamin.gaude@gammeo.com>
2024-11-20 10:07:20 +0100

    chore: update workspace attribution & icons + nvim plugin conform

---
+++ config/nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,37 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {},
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        css = { "prettier" },
+        graphql = { "prettier" },
+        json = { "prettier" },
+        html = { "prettier" },
+        liquid = { "prettier" },
+        markdown = { "prettier" },
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        yaml = { "prettier" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+      conform.format({
+        lsp_format = "fallback",
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
+}
